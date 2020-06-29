6e9bc2216fc2a58ddbd05f6fc0d3b54fc9ef6f80
Gareth La Grange <glagrange@gmail.com>
2020-06-29 15:04:48 -0400

    Update py-mstr-syncconfig.py
    added logging

---
+++ py-mstr-syncconfig.py
@@ -0,0 +1,28 @@
+import logging
+from logging.handlers import RotatingFileHandler
+from logging import handlers
+import sys
+
+#set logging level
+## Use filename as variale
+## Sets level to INFO and sets format to be used for file and console
+log = logging.getLogger(__name__) 
+log.setLevel(logging.INFO)
+format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+# Set logging to Console
+ch = logging.StreamHandler(sys.stdout)
+ch.setFormatter(format)
+log.addHandler(ch)
+
+# Set logging to file, with size and rotation
+fh = handlers.RotatingFileHandler('logfile2.log', maxBytes=(1048576*5), backupCount=7)
+fh.setFormatter(format)
+log.addHandler(fh)
+
+# Logs test
+log.debug('A debug message')
+log.info('An info message')
+log.warning('Something is not right.')
+log.error('A Major error has happened.')
+log.critical('Fatal error. Cannot continue')
