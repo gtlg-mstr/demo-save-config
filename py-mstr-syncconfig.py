cc9fc914c3d0323ce4730b312eb9e4dc15448900
Gareth La Grange <glagrange@gmail.com>
2020-06-29 15:51:18 -0400

    test config

--- py-mstr-syncconfig.py
+++ py-mstr-syncconfig.py
@@ -3,7 +3,9 @@ from logging.handlers import RotatingFileHandler
 from logging import handlers
 import sys
 import requests, json #For MSTR API ACCESS
-import yaml 
+#import yaml 
+# YAML Intro https://stackabuse.com/reading-and-writing-yaml-to-a-file-in-python/
+import configparser
 
 
 #set logging level
@@ -30,3 +32,44 @@ log.addHandler(fh)
 # log.error('A Major error has happened.')
 # log.critical('Fatal error. Cannot continue')
 
+log.info('reading config file')
+##Read config files
+Config = configparser.ConfigParser()
+Config.read("config.ini")
+ConfigSections = Config.sections()
+
+print(ConfigSections)
+
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+log.info('Checking section')
+#base_url = ConfigSectionMap("server")['serverid']
+
+
+
+
+#### FUNCTIONS ###
+def login(base_url,api_login,api_password):
+    print("Getting token...")
+    data_get = {'username': api_login,
+                'password': api_password,
+                'loginMode': 1}
+    r = requests.post(base_url + 'auth/login', data=data_get)
+    if r.ok:
+        authToken = r.headers['X-MSTR-AuthToken']
+        cookies = dict(r.cookies)
+        print("\nToken: " + authToken)
+        return authToken, cookies
+    else:
+        print("HTTP %i - %s, Message %s" % (r.status_code, r.reason, r.text))
